cbce90c6677fb049b82670685c09ebcb30d869bd
Evgenij Beloded <belodedevgenij@gmail.com>
2017-11-09 10:08:59 +0700

    Update app structure

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+export class App extends React.Component {
+  render () {
+    return (
+      <div className="earth-wrap">
+      Hello        
+      </div>
+    );
+  }
+}
